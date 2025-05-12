f2162a8389762cc18694e5fe8244b7bcfedcbc15
Ivan Volkov <volkov.ivan2004@gmail.com>
2025-05-12 13:08:01 +0300

    Basic implementation of storage (not thread-safe)

--- src/storage.h
+++ src/storage.h
@@ -2,20 +2,61 @@
 
 template <typename KeyT, typename ValueT>
 class Storage {
+private:
+    struct StorageCell {
+        KeyT key;
+        ValueT value;
+        bool used;
+    };
+
+    int N, K;
+    StorageCell *storage;
+
 public:
-    Storage(int N, int K) {}
+    Storage(int N, int K): N(N), K(K) {
+        storage = new StorageCell[N];
+    }
 
     void store(KeyT key, ValueT value) {
-        // TODO
+        int num = std::hash<KeyT>{}(key) % N;
+        int i = num;
+        do {
+            if (storage[i].key == key) {
+                storage[i].value = value;
+                return;
+            }
+            if (!storage[i].used) {
+                storage[i].key = key;
+                storage[i].value = value;
+                storage[i].used = true;
+                return;
+            }
+            i = (i + 1) % N;
+        } while (i != num);
+        assert(false);
     }
 
     ValueT load(KeyT key) {
-        // TODO
-        return 0;
+        int num = std::hash<KeyT>{}(key) % N;
+        int i = num;
+        do {
+            if (storage[i].key == key) {
+                return storage[i].value;
+            }
+            if (!storage[i].used) {
+                assert(false);
+            }
+            i = (i + 1) % N;
+        } while (i != num);
+        assert(false);
     }
 
     bool updateIfEquals(KeyT key, ValueT oldValue, ValueT newValue) {
         // TODO
         return false;
+    }
+
+    ~Storage() {
+        delete[] storage;
     }
 };
